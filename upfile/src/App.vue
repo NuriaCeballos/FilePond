e77b4ed5c6bd2736e78cb3b26b01cd0c2b58ef08
Nuria <71881401+NuriaCeballos@users.noreply.github.com>
2021-09-08 09:54:35 +0200

    subir archivos

---
+++ upfile/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Subir Archivos</router-link> 
+    
+  </div>
+  <div id="app">
+    <FilePond allowMultiple="true" />
+  </div>
+  <router-view/>
+</template>
+
+<script>
+import vueFilePond from 'vue-filepond'
+import 'filepond/dist/filepond.min.css'
+
+import FilePondPluginImagePreview from 'filepond-plugin-image-preview'
+import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.min.css'
+
+export default {
+    name: 'app',
+    components: {
+        FilePond: vueFilePond(FilePondPluginImagePreview)
+    }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
